8f55078f15dbb7be3a6e3f2fe823b857718ac48f
mtcxl <chenlong@meituan.com>
2017-01-17 19:59:10 +0800

    This is my first git project.

---
+++ src/Test.java
@@ -0,0 +1,10 @@
+/**
+ * Created by mt_cxl on 2017/1/13.
+ */
+public class Test {
+    public static  void main(String agrs[])
+    {
+        System.out.println("hello world");
+        System.out.println("test");
+    }
+}
